65d8b4bb607f54c9bbaabc63fd4a564e811bd47a
Hieu Tran <hieu@buuuk.com>
2022-12-23 19:49:30 +0700

    added script using origin

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,3 +5,8 @@
 npx husky-init && yarn
 
 npx mrm@2 lint-staged
+
+# remove current origin
+current_origin=$(git remote get-url origin)
+
+[ "$current_origin" == "git@github.com:hieutran21198/react-vite-cypress-kit.git" ] || [ "$current_origin" == "https://github.com/hieutran21198/react-vite-cypress-kit.git" ] && git remote remove origin
